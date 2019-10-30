fa218046d03a254ed878a4121cc71fb53420995c
Antonio Martinez Diaz <teclado1333@gmail.com>
2019-10-30 13:53:04 +0100

    proyectoPmd

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProveedoresService } from './servicios/proveedores.service';
+import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent}
+   ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProveedoresComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+
+  
+  providers: [ProveedoresService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
